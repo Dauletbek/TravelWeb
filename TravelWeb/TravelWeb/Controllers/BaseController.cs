f3fff36319be12653dd44ec890596c4548fcb759
jarkinbek.j <j_jaha28@yahoo.com>
2017-05-20 17:28:55 +0800

    Base controller

---
+++ TravelWeb/TravelWeb/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using TravelWeb.DAL;
+using System.Linq;
+
+namespace TravelWeb.Controllers
+{
+    public class BaseController : Controller
+    {
+        private TravelContext db = new TravelContext();
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            ViewBag.Submenus = db.TravelTypes.ToList();
+        }
+    }
+}
